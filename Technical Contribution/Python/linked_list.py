2bec16fef8566cc4ba2331296ae0e6611360d33a
Ritik Raj <33518732+chronos25@users.noreply.github.com>
2019-04-18 10:15:56 +0530

    Fix me 1.py

---
+++ Technical Contribution/Python/linked_list.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,data=None):
+        self.data=data
+        self.next=None
+        
+class Slinkedlist:
+    def __init__(self):
+        self.headval=None
+    def listprint(self):
+        printval =self.headval
+        while printval is not None:
+            
+
+list1=Slinkedlist()
+list1.headval=Node(1)
+e2=Node(2)
+e3=Node(3)
+
+#link 1st node to 2nd node
+ 
+
+#link 2nd node to 3rd node
+
+
+
